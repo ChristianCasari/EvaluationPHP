97d9ebb28b5d94286ed02132b58d8cf4f6d3243b
ChristianCasari <148464871+ChristianCasari@users.noreply.github.com>
2024-02-03 21:03:04 +0000

    UpdateFev2024Php

--- addProduct.php
+++ addProduct.php
@@ -19,7 +19,7 @@ require_once ('Views/header.php');
     <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="price">
   </div>
 
-  <!-- Ajout du paramètre image (colonne supplémentaire dans la table avec l'accès pour chaque image) -->
+  <!--Ajout du paramètre image (colonne supplémentaire dans la table avec l'accès pour chaque image)-->
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Accès à l'image</label>
     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="image">
